808217e38e9ce631d1b4434c9345613e66961fe2
wanmei523 <gogowlm@163.com>
2020-11-10 23:09:39 +0800

    学生界面完成，课程完结

--- app/Models/ResourceVideo.php
+++ app/Models/ResourceVideo.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Models;
-
+use AliCloud\Core\Profile\DefaultProfile;
+use AliCloud\Core\DefaultAcsClient;
+use AliCloud\Vod\GetVideoPlayAuthRequest;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -9,5 +11,28 @@ class ResourceVideo extends Model
 {
     //
     use SoftDeletes;
-    protected $fillable=['resource_id','ali_id'];
+    protected $fillable = ['resource_id', 'ali_id'];
+
+    public function getPlayAuthAttribute()
+    {
+        //使用
+        $access_key_id = setting('ali_access');
+        $access_key_secret = setting("ali_secret");
+        $region_id = setting('ali_region');
+
+        try {
+            $profile = DefaultProfile::getProfile($region_id, $access_key_id, $access_key_secret);
+
+            $client = new DefaultAcsClient($profile);
+
+            $videoId = $this->ali_id;
+            $req = new GetVideoPlayAuthRequest();
+            $req->setVideoId($videoId);
+            $req->setAcceptFormat("JSON");
+            $response = $client->getAcsResponse($req);
+        } catch (Exception $e) {
+            exit($e);
+        }
+        return $response;
+    }
 }
--- resources/views/index/course/video.blade.php
+++ resources/views/index/course/video.blade.php
@@ -1,3 +1,33 @@
 <div style="color:#212529">
-{{$resource->resourceVideo->ali_id}}
+
 </div>
+<div class='prism-palyer' id='player-con'></div>
+@section('css')
+<link rel="stylesheet" href="https://g.alicdn.com/de/prismplayer/2.9.1/skins/default/aliplayer-min.css" />
+@endsection
+
+@section('js')
+<script type="text/javascript" charset="utf-8" src="https://g.alicdn.com/de/prismplayer/2.9.1/aliplayer-min.js"></script>
+<script>
+var player = new Aliplayer({
+  "id": "player-con",
+  "vid": "{{$resource->resourceVideo->ali_id}}",
+  "playauth": "{{$resource->resourceVideo->playauth->PlayAuth}}",
+  "qualitySort": "asc",
+  "format": "mp4",
+  "mediaType": "video",
+  "width": "100%",
+  "height": "500px",
+  "autoplay": false,
+  "isLive": false,
+  "rePlay": false,
+  "playsinline": true,
+  "preload": true,
+  "controlBarVisibility": "hover",
+  "useH5Prism": true
+}, function (player) {
+    console.log("The player is created");
+  }
+);
+</script>
+@endsection
